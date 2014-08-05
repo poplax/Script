af5c2727120b70e2533b879e6b5c3a1ea2e401c6
LinJiang <linjiang9999@gmail.com>
2014-08-05 14:25:09 +0800

    add Photoshop Script 2x.

---
+++ photoshop_Scripts/LinTest.jsx
@@ -0,0 +1,58 @@
+﻿#target photoshop
+
+main();
+
+function main ()
+{
+	if (0 < app.documents.length)
+	{
+		alert ("请关闭所有打开的文件");
+		return;
+	}
+
+	// Use folder selection dialogs to get the location of the input files
+	// and where to save the new output files.
+	var sourceFolder = Folder.selectDialog ("Please choose the location of the source image files.", Folder.myDocuments);
+	var destFolder = Folder.selectDialog ("Please choose a location where the new image files will be saved.", sourceFolder);
+
+	var files = sourceFolder.getFiles();
+	for (var i = 0; i < files.length; i++)
+	{
+		var f = files[i];
+		if (f instanceof Folder)
+			continue;
+
+		// If there are no other documents open doc is the active document.
+		var doc = app.open (f);
+        
+		var layer = doc.artLayers.add ();
+		layer.bounds = [0,0,doc.width,doc.height];
+
+		// Now make the layer into a text layer and set parameters.
+		// The text will be centered, in the hideous default font, and white.
+		// Note that font size depends not just on the point size, but also
+		// on the resolution, which is NOT being set anywhere.
+		layer.kind = LayerKind.TEXT;
+		layer.textItem.position = [Math.round (doc.width / 2),Math.round (doc.height / 2)];
+		layer.textItem.justification = Justification.CENTER;
+		layer.textItem.color.rgb.hexValue = "FFFFFF";
+		layer.textItem.size = 60;
+
+		// Get the file name and set it as the text this assumes the full path is not wanted.
+		// to set the full path swap fsname for name.
+        
+		layer.textItem.contents = File.decode (f.name);
+
+		doc.flatten ();
+
+		// Save as a new JPG file with these options.
+		var options = new JPEGSaveOptions ();
+		options.quality = 8;
+
+		var outputFile = new File (destFolder.absoluteURI + "/" + f.name);
+		doc.saveAs (outputFile, options, false, Extension.LOWERCASE);
+
+		doc.close ();
+	}
+}
+
---
+++ photoshop_Scripts/Resize2x.jsx
@@ -0,0 +1,42 @@
+﻿/*
+   Photoshop Script ResizeRetinaToNormal.jsx
+   Finds @2x.png image files for retina display in the specified folder and 
+   resize them to the half size for normal display.
+*/
+
+// Keep the original unit and change it to pixcel
+var strtRulerUnits = preferences.rulerUnits;
+preferences.rulerUnits = Units.PIXELS;
+
+// Select folder
+var sourceFolder = Folder.selectDialog("请选择包含有\"@2x\"大图的文件夹.");
+var destFolder = Folder.selectDialog ("请选择裁剪后小图需要存放的文件:", sourceFolder);
+
+var fileList = sourceFolder.getFiles("*@2x.png");
+for (var s in fileList){
+    var file = fileList[s];
+    open(file);
+
+    // resize image
+    activeDocument.resizeImage(
+        activeDocument.width / 2.0,
+        activeDocument.height / 2.0,
+        activeDocument.resolution,
+        ResampleMethod.BICUBIC);
+    
+    var resizedFileName = activeDocument.name.match(/(.*)\@2x.[^\.]+$/)[1];
+
+    // save
+    var saveFile = new File(destFolder.absoluteURI + "/" + resizedFileName + ".png");
+
+    var webSaveOptions = new ExportOptionsSaveForWeb();
+    webSaveOptions.format = SaveDocumentType.PNG;
+    webSaveOptions.PNG8 = false;
+    webSaveOptions.transparency = true; 
+    activeDocument.exportDocument(saveFile, ExportType.SAVEFORWEB, webSaveOptions);
+
+    activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+}
+
+// Revert the unit preference
+preferences.rulerUnits = strtRulerUnits;
